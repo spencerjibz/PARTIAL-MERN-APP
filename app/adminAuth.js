8b5af739d33185b3ac4e04dfd6ee2869ae7f2018
spencerjibz <spencernajib2@gmail.com>
2018-11-18 03:31:47 +0000

    modified:   app/adminAuth.js 	modified:   config/keys.js 	deleted:    lib/filemanger.js

--- app/adminAuth.js
+++ app/adminAuth.js
@@ -4,8 +4,8 @@ let  admin =(req,res,next)=>{
 let {log} = console
 
 let {username} =res.locals.user
-
-const exp = username.includes('najib2')
+// enter admin name in includes
+const exp = username.includes('')
 
 if(exp!==true){
 req.flash('error_msg','not authorized  as admin')
--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,12 @@
 module.exports ={
-    prod: process.env.DBURI|| 'mongodb://merns:mernstack18@ds024748.mlab.com:24748/myusers',
-    home:'mongodb://localhost:27017/myusers',
-    EA: process.env.EA||'merns20@gmail.com',
-    pass: process.env.pass||'mern@2018',
+    // enter the uri for production mongodb 
+    prod: process.env.DBURI|| 'mongodb://<username>:<password without special characters></passwordwithout>@ds024748.mlab.com:24748/myusers',
+    // uri for mongodb installed locally, enter the database name
+    home:'mongodb://localhost:27017/databasename',
+    // enter your email address below
+    EA: process.env.EA||'example@domain.com',
+    // enter your password for nodemailer auth
+    pass: process.env.pass||'password',
+    //enter the port of your app 
     PORT:process.env.PORT||9000
 }
